b85a85d5e13afc8696687bf38717c6c8bb79b797
hyrodium <hyrodium@gmail.com>
2020-02-11 21:20:10 +0900

    update paraboloid

--- Examples/Paraboloid.jl
+++ Examples/Paraboloid.jl
@@ -1,18 +1,18 @@
 using Distributed
-addprocs(13);
+addprocs(1);
 @everywhere push!(LOAD_PATH, "Modules")
-@everywhere push!(LOAD_PATH, ".")
 using IntervalSets
 using Printf
 using Bspline
 using I4SM
 
 @DefineShape 𝒑₍₀₎(u)=[u...,u'*(u.*[-1.0,1.0])]
-D=((-1.0)..1.0,0.0..0.1)
-Settings("Paraboloid-1a",up=3,down=-3,right=3,left=-3,mesh=(20,1),unit=200,slack=true)
+n=10
+id=1
 
+D=(-1.0..1.0, (i-1)/n..i/n)
+Settings("Paraboloid-"*(@sprintf "%02d" id), up=3,down=-3,right=3,left=-3,mesh=(20,1),unit=200,slack=true)
 InitialConfiguration(D)
-
 fixed(n₁,n₂)=[[1,(n₂+1)÷2,1],[1,(n₂+1)÷2,2],[n₁,(n₂+1)÷2,1],[n₁,(n₂+1)÷2,2],[(n₁+1)÷2,(n₂+1)÷2,1],[(n₁+1)÷2,(n₂+1)÷2,2],[(n₁+1)÷2,(n₂+1)÷2-1,1]]
 NewtonMethodIteration(nip=25,fixed=fixed)
 NewtonMethodIteration(nip=45)
