a3b3840f2d9e9212c90bf6182d83e512de6cfe96
hyrodium <hyrodium@gmail.com>
2021-08-14 01:54:12 +0900

    Runge-Kutta method

--- src/_InitialConfiguration.jl
+++ src/_InitialConfiguration.jl
@@ -1,46 +1,89 @@
-using DifferentialEquations
-
 export InitialConfiguration
-function InitialConfiguration(D; n₁ = 15, nip = NIP)
+function InitialConfiguration(D; n₁ = 15)
     parent = 0
 
     D₁, D₂ = D
-    M = InitBs(D, n₁, nip = nip)
+    M = _initialize(D, n₁)
     comment = "Initial Configuration - domain: " * repr([endpoints(D₁)...]) * "×" * repr([endpoints(D₂)...])
 
     Export(M, parent, comment = comment)
 end
 
-function InitBs(D, n₁; nip = NIP)
+function _initialize(D, n₁)
     D₁, D₂ = D
 
-    function ode(𝒄̇𝒄̈, 𝒄𝒄̇, par, t)
-        𝒄̇𝒄̈[1] = 𝒄𝒄̇[3]
-        𝒄̇𝒄̈[2] = 𝒄𝒄̇[4]
-        𝒄̇𝒄̈[3] = dot([s̈₍₀₎(D₂, t) / ṡ₍₀₎(D₂, t), -𝜅₍₀₎(D₂, t) * ṡ₍₀₎(D₂, t)], 𝒄𝒄̇[3:4])
-        𝒄̇𝒄̈[4] = dot([𝜅₍₀₎(D₂, t) * ṡ₍₀₎(D₂, t), s̈₍₀₎(D₂, t) / ṡ₍₀₎(D₂, t)], 𝒄𝒄̇[3:4])
-    end
-    𝒄𝒄̇₀ = vcat([0.0, 0.0], [1.0, 0.0] * ṡ₍₀₎(D₂, minimum(D₁)))
-    curve = solve(ODEProblem(ode, 𝒄𝒄̇₀, extrema(D₁)))
-    𝒄(t) = curve(t)[1:d] # center curve of the solution
-    𝒄₁(t) = curve(t)[(1:d).+d] # its derivative
-    𝒄₂(t) = [g₍₀₎₁₂(c(D₂, t)) -𝝊₍₀₎(c(D₂, t)); 𝝊₍₀₎(c(D₂, t)) g₍₀₎₁₂(c(D₂, t))] * 𝒄₁(t) / g₍₀₎₁₁(c(D₂, t)) # 中心曲線上の幅方向のベクトル場
-
-    D₁₋, D₁₊ = extrema(D₁)
+    # Definition for center curve
+    t₋, t₊ = extrema(D₁)
     p₁ = 3
-    k₁ = Knots(range(D₁₋, D₁₊, length = n₁ - p₁ + 1)) + p₁ * Knots(D₁₋, D₁₊)
-    P₁ = FastBSplineSpace(p₁, k₁)
-
-    𝒎 = fittingcontrolpoints(t -> 𝒄(t[1]), [P₁])
-    𝒓 = fittingcontrolpoints(t -> 𝒄₂(t[1]), [P₁])
-    a1 = 𝒎 - width(D₂) * 𝒓 / 2
-    a2 = 𝒎 + width(D₂) * 𝒓 / 2
     p₂ = 1
+    k₁ = Knots(range(t₋, t₊, length = n₁-p₁+1)) + p₁ * Knots(t₋, t₊)
     k₂ = Knots(repeat(collect(extrema(D₂)), inner = 2))
-    n₂ = length(k₂) - p₂ - 1
-
+    P₁ = FastBSplineSpace(p₁, k₁)
     P₂ = FastBSplineSpace(p₂, k₂)
-    𝒂 = hcat(a1, a2)
+    n₂ = dim(P₂)
+
+
+    # Number of divisions for ODE
+    N = 128
+
+    # Solve 𝒄̈(t) = A(t)𝒄̇(t) with Runge-Kutta method
+    A(t) = [s̈₍₀₎(D₂, t) / ṡ₍₀₎(D₂, t) -𝜅₍₀₎(D₂, t) * ṡ₍₀₎(D₂, t)
+    𝜅₍₀₎(D₂, t) * ṡ₍₀₎(D₂, t) s̈₍₀₎(D₂, t) / ṡ₍₀₎(D₂, t)]
+
+    # Initial condition
+    𝒄̇₀ = [1.0, 0.0] * ṡ₍₀₎(D₂, minimum(D₁))
+
+    Δt = (t₊-t₋)/N
+    ts = range(t₋, stop=t₊, length=N+1)
+    𝒄̇s = zeros(N+1,2)
+    𝒄̇s[1,:] = 𝒄̇₀
+    for i in 1:N
+        t = ts[i]
+        𝒄̇ = 𝒄̇s[i,:]
+
+        k1 = A(t)*𝒄̇
+        k2 = A(t+Δt/2)*(𝒄̇+k1*Δt/2)
+        k3 = A(t+Δt/2)*(𝒄̇+k2*Δt/2)
+        k4 = A(t+Δt)*(𝒄̇+k3*Δt)
+
+        Δ𝒄̇₀ = Δt * (k1+2k2+2k3+k4)/6
+        𝒄̇s[i+1,:] = 𝒄̇ + Δ𝒄̇₀
+    end
+
+    # Approximate 𝒄̇=𝒄₁ with B-spline curve
+    _p₁ = p₁-1
+    _k₁ = Knots(range(t₋, t₊, length = n₁-_p₁)) + _p₁ * Knots(t₋, t₊)
+    _P₁ = FastBSplineSpace(_p₁, _k₁)
+    _n₁ =  dim(_P₁)
+    _B = [bsplinebasis(i, _P₁, t) for i in 1:_n₁, t in ts]
+    _BB = _B * _B'
+    _b = _B * 𝒄̇s
+    𝒎̇ = _BB\_b  # control points of 𝒄̃₁
+
+    # Approximate 𝒄 with B-spline curve
+    Δk = (t₊-t₋)/(n₁-p₁)
+    𝒎 = zeros(dim(P₁),2)  # control points of 𝒄̃
+    𝒎[1,:] = zeros(2)
+    𝒎[2,:] = 𝒎[1,:] + 𝒎̇[1,:]*Δk*1/3
+    𝒎[3,:] = 𝒎[2,:] + 𝒎̇[2,:]*Δk*2/3
+    for i in 3:dim(P₁)-1
+        𝒎[i+1,:] = 𝒎[i,:] + 𝒎̇[i,:]*Δk
+    end
+    𝒎[n₁-1,:] = 𝒎[n₁-2,:] + 𝒎̇[n₁-2,:]*Δk*2/3
+    𝒎[n₁,:] = 𝒎[n₁-1,:] + 𝒎̇[n₁-1,:]*Δk*1/3
+
+    # Approximate 𝒄₂ with B-spline curve
+    𝒄₂s = [[g₍₀₎₁₂(c(D₂, ts[i])) -𝝊₍₀₎(c(D₂, ts[i])); 𝝊₍₀₎(c(D₂, ts[i])) g₍₀₎₁₂(c(D₂, ts[i]))] * 𝒄̇s[i,:] / g₍₀₎₁₁(c(D₂, ts[i])) for i in 1:N+1]
+    𝒄₂s = hcat(𝒄₂s...)'
+
+    _B = [bsplinebasis(i, P₁, t) for i in 1:n₁, t in ts]
+    _BB = _B * _B'
+    _b = _B * 𝒄₂s
+    𝒓 = _BB\_b  # control points of 𝒄̃₂
+
+    a1 = 𝒎 - width(D₂) * 𝒓/2
+    a2 = 𝒎 + width(D₂) * 𝒓/2
+    𝒂 = [(a1[I₁,i],a2[I₁,i])[I₂] for I₁ in 1:n₁, I₂ in 1:n₂, i in 1:2]
 
     M = BSplineSurface([P₁, P₂], 𝒂)
     M′ = refinement(M, p₊ = [0, 1])
