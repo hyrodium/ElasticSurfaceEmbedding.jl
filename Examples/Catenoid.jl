589fd23c9663023952e743d43fe22832a9f938ec
hyrodium <hyrodium@gmail.com>
2021-09-09 17:40:30 +0900

    remove examples

--- Examples/Catenoid.jl
+++
@@ -1,8 +0,0 @@
-using IntervalSets
-using BSpline
-using ElasticSurfaceEmbedding
-
-n=9
-domain = (-π/2..π/2,-π/(4n)..π/(4n))
-@parametric_mapping 𝒑₍₀₎(u)=[cos(u[2])*cosh(u[1]),sin(u[2])*cosh(u[1]),u[1]]
-@parametric_mapping 𝒑₍₀₎(u)=[cos(u[1])*cosh(u[2]),sin(u[1])*cosh(u[2]),u[2]]
--- Examples/Helicoid.jl
+++
@@ -1,8 +0,0 @@
-using IntervalSets
-using BSpline
-using ElasticSurfaceEmbedding
-
-n=9
-domain = (-π/2..π/2,-π/(4n)..π/(4n))
-@parametric_mapping 𝒑₍₀₎(u)=[cos(u[2])*sinh(u[1]),sin(u[2])*sinh(u[1]),u[2]]
-@parametric_mapping 𝒑₍₀₎(u)=[cos(u[1])*sinh(u[2]),sin(u[1])*sinh(u[2]),u[1]]
