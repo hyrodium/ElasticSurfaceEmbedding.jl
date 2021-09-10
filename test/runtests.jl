0546b86962b3c746e6ec959e912be5f8b246060b
hyrodium <hyrodium@gmail.com>
2021-09-11 03:25:02 +0900

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using IntervalSets
 using BasicBSpline
 using ElasticSurfaceEmbedding
 using Images
+using LinearAlgebra
 
 dir_result_a = joinpath(@__DIR__, "result_a")
 dir_result_b = joinpath(@__DIR__, "result_b")
@@ -10,26 +11,89 @@ dir_result_b = joinpath(@__DIR__, "result_b")
 rm(dir_result_b, recursive=true, force=true)
 config_dir(dir_result_b)
 
-@testset "ElasticSurfaceEmbedding.jl" begin
-    @testset "Paraboloid" begin
-        @parametric_mapping 𝒑₍₀₎(u) = [u...,u'*u]
-        D(i,n) = (-1.0..1.0, (i-1)/n..i/n)
-        name = "Paraboloid"
-        settings(name,canvas=(4,4),mesh=(20,1),unit=200,colorbarsize=0.3)
+@testset "Rhomboid" begin
+    @parametric_mapping 𝒑₍₀₎(u) = [u...,u[1]+u[2]]
+    D = (-1.0..1.0, -1.0..1.0)
+    name = "Rhomboid"
+    settings(name,canvas=(4,4),mesh=(20,1),unit=200,colorbarsize=0.3)
 
-        i=3
-        initial_state(D(i,10))
+    initial_state(D, n₁=5)
+    M = ElasticSurfaceEmbedding.loadM()
+    𝒂 = controlpoints(M)
+    @test 𝒂[1,1,:] ≈ [-√(3/2), -3/√(2)]
+    @test 𝒂[1,2,:] ≈ [-√(3/2), -1/√(2)]
+    @test 𝒂[1,3,:] ≈ [-√(3/2), 1/√(2)]
+    @test 𝒂[3,1,:] ≈ [0, -2/√(2)]
+    @test norm(𝒂[3,2,:]) < 1e-14
+    @test 𝒂[3,3,:] ≈ [0, 2/√(2)]
+    @test 𝒂[5,1,:] ≈ [√(3/2), -1/√(2)]
+    @test 𝒂[5,2,:] ≈ [√(3/2), 1/√(2)]
+    @test 𝒂[5,3,:] ≈ [√(3/2), 3/√(2)]
 
-        newton_onestep(fixingmethod=:fix3points)
-        newton_onestep()
-        spline_refinement(p₊=[0,1],k₊=[Knots(),Knots([(i-1/2)/10])])
-        newton_onestep()
-        newton_onestep()
-        add_pin(tag="$name-$i")
+    newton_onestep()
+    M = ElasticSurfaceEmbedding.loadM()
+    𝒂 = controlpoints(M)
+    @test 𝒂[1,1,:] ≈ [-√(3/2), -3/√(2)]
+    @test 𝒂[1,2,:] ≈ [-√(3/2), -1/√(2)]
+    @test 𝒂[1,3,:] ≈ [-√(3/2), 1/√(2)]
+    @test 𝒂[3,1,:] ≈ [0, -2/√(2)]
+    @test norm(𝒂[3,2,:]) < 1e-14
+    @test 𝒂[3,3,:] ≈ [0, 2/√(2)]
+    @test 𝒂[5,1,:] ≈ [√(3/2), -1/√(2)]
+    @test 𝒂[5,2,:] ≈ [√(3/2), 1/√(2)]
+    @test 𝒂[5,3,:] ≈ [√(3/2), 3/√(2)]
+end
 
-        img_a = load(joinpath(dir_result_a,"Paraboloid","append","Paraboloid-5_append.png"))
-        img_b = load(joinpath(dir_result_b,"Paraboloid","append","Paraboloid-5_append.png"))
-        d = Euclidean()
-        @test d(img_a, img_b) < 0.0001
-    end
+@testset "Rhomboid" begin
+    @parametric_mapping 𝒑₍₀₎(u) = [u...,u[1]+u[2]]
+    D = (-1.0..1.0, -1.0..1.0)
+    name = "Rhomboid"
+    settings(name,canvas=(4,4),mesh=(20,1),unit=200,colorbarsize=0.3)
+
+    initial_state(D, n₁=5)
+    M = ElasticSurfaceEmbedding.loadM()
+    𝒂 = controlpoints(M)
+    @test 𝒂[1,1,:] ≈ [-√(3/2), -3/√(2)]
+    @test 𝒂[1,2,:] ≈ [-√(3/2), -1/√(2)]
+    @test 𝒂[3,1,:] ≈ [0, -2/√(2)]
+    @test norm(𝒂[3,2,:]) < 1e-14
+    @test 𝒂[3,3,:] ≈ [0, 2/√(2)]
+    @test 𝒂[5,1,:] ≈ [√(3/2), -1/√(2)]
+    @test 𝒂[5,2,:] ≈ [√(3/2), 1/√(2)]
+    @test 𝒂[5,3,:] ≈ [√(3/2), 3/√(2)]
+
+    newton_onestep()
+    M = ElasticSurfaceEmbedding.loadM()
+    𝒂 = controlpoints(M)
+    @test 𝒂[1,1,:] ≈ [-√(3/2), -3/√(2)]
+    @test 𝒂[1,2,:] ≈ [-√(3/2), -1/√(2)]
+    @test 𝒂[1,3,:] ≈ [-√(3/2), 1/√(2)]
+    @test 𝒂[3,1,:] ≈ [0, -2/√(2)]
+    @test norm(𝒂[3,2,:]) < 1e-14
+    @test 𝒂[3,3,:] ≈ [0, 2/√(2)]
+    @test 𝒂[5,1,:] ≈ [√(3/2), -1/√(2)]
+    @test 𝒂[5,2,:] ≈ [√(3/2), 1/√(2)]
+    @test 𝒂[5,3,:] ≈ [√(3/2), 3/√(2)]
+end
+
+@testset "Paraboloid" begin
+    @parametric_mapping 𝒑₍₀₎(u) = [u...,u'*u]
+    D(i,n) = (-1.0..1.0, (i-1)/n..i/n)
+    name = "Paraboloid"
+    settings(name,canvas=(4,4),mesh=(20,1),unit=200,colorbarsize=0.3)
+
+    i=3
+    initial_state(D(i,10))
+
+    newton_onestep(fixingmethod=:fix3points)
+    newton_onestep()
+    spline_refinement(p₊=[0,1],k₊=[Knots(),Knots([(i-1/2)/10])])
+    newton_onestep()
+    newton_onestep()
+    add_pin(tag="$name-$i")
+
+    img_a = load(joinpath(dir_result_a,"Paraboloid","append","Paraboloid-5_append.png"))
+    img_b = load(joinpath(dir_result_b,"Paraboloid","append","Paraboloid-5_append.png"))
+    d = Euclidean()
+    @test d(img_a, img_b) < 0.0001
 end
