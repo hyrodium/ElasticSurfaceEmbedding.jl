4a8567089a140721aa9a4c298ce82f10a8999689
hyrodium <hyrodium@gmail.com>
2023-08-07 12:48:23 +0900

    remove unnecessary const

--- src/_constants.jl
+++ src/_constants.jl
@@ -4,5 +4,3 @@ const 𝝂 = 0.25 # Poisson's Ratio
 const Y = 1.0 # Young Modulus
 const 𝝀 = 𝝂 * Y / ((1 + 𝝂) * (1 - (d - 1) * 𝝂)) # Lamé constant
 const 𝝁 = 1 / 2(1 + 𝝂) # Lamé constant
-
-const ESE_VERSION = v"0.0.1"
